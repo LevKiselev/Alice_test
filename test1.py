d5ff7f9ae7244271944ba71a31703c0b8570464a
ocnit <klen@tula.ru>
2026-03-26 12:01:10 +0300

    Logging tests add

---
+++ test1.py
@@ -0,0 +1,12 @@
+import logging
+
+# logging.basicConfig(filename='test1.log')
+# logging.basicConfig(filename='test1.log', format='%(asctime)s %(levelname)s %(name)s %(message)s')
+
+def log():
+    for i in range(10):
+        logging.warning(i)
+
+
+if __name__ == '__main__':
+    log()
